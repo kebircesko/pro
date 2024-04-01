ddd3a832c11e431d19c57be69699c7e8b071586e
Tarik Ajvazi <tarikajvazi2@gmail.com>
2024-04-01 15:36:45 +0200

    headercategories

--- src/components/CategoriesFilter/CategoriesFilter.jsx
+++ src/components/CategoriesFilter/CategoriesFilter.jsx
@@ -13,6 +13,9 @@ import cacttus from "./../../assets/CategoriesFilter/cacttus.svg"
 import ubt from "./../../assets/CategoriesFilter/ubt.svg"
 import innovemia from "./../../assets/CategoriesFilter/innovemia.svg"
 import cacttuspng from "./../../assets/CategoriesFilter/cacttus.png"
+import adaptech from "./../../assets/CategoriesFilter/adaptech.svg"
+import eizek from "./../../assets/CategoriesFilter/eizek.svg"
+import hangar from "./../../assets/CategoriesFilter/hangar.svg"
 import { IoMdMenu, IoIosArrowForward, IoIosArrowDown, IoIosArrowBack,IoIosSearch, IoIosClose } from "react-icons/io";
 import { FaFacebookF, FaInstagram, FaLinkedin } from "react-icons/fa";
 
@@ -45,6 +48,25 @@ const CategoriesFilter = () => {
         setShowTrainings(false);
     }
 
+    const [headerCategories, setHeaderCategories] = useState(false);
+
+    const toggleHeaderCategories = () => {
+        if(headerSub){
+            setHeaderSub(false);
+            setHeaderCategories(false);
+        }
+        else{
+            setHeaderCategories(!headerCategories);
+        }
+    }
+
+    const [headerSub, setHeaderSub] = useState(false);
+
+    const toggleHeaderSub = () => {
+        setHeaderSub(!headerSub);
+        setHeaderCategories(!headerCategories);
+    }
+
     return (
         <>
             <header className='headermobile'>
@@ -55,9 +77,98 @@ const CategoriesFilter = () => {
                 <br />
                 <h1>Explore and navigate around the Innovation and Training Park</h1>
                 <div className="searchbox">
-                    <button className='dropdownsearch'><IoIosArrowDown className="arrowdownicon"/></button>
+                    <button className={`dropdownsearch ${headerCategories || headerSub ? ' activearrow' : ''}`} onClick={toggleHeaderCategories}>
+                        {
+                            headerCategories ? <IoIosArrowForward className="arrowdownicon"/> : <IoIosArrowDown className="arrowdownicon"/>
+                        }
+                    </button>
                     <input type="search" placeholder='Search by name, building, event' />
-                    <button className='searchbutton' type='submit'><IoIosSearch className="searchicon"/></button>
+                    <button className={`searchbutton ${headerCategories ? 'activesearch' : ''}`} type='submit'><IoIosSearch className="searchicon"/></button>
+                </div>
+                <div className={`headercategories ${headerCategories ? 'fade-in' : ''}`}>
+                    <div onClick={toggleHeaderSub}>
+                        <IoIosArrowForward />
+                        <p>ICT & Technology</p>
+                    </div>
+                    <div>
+                        <IoIosArrowForward />
+                        <p>Education & Trainings</p>
+                    </div>
+                    <div>
+                        <IoIosArrowForward />
+                        <p>Research & Development</p>
+                    </div>
+                    <div>
+                        <IoIosArrowForward />
+                        <p>Green Energy & Environment</p>
+                    </div>
+                    <div>
+                        <IoIosArrowForward />
+                        <p>Creative & Culture</p>
+                    </div>
+                    <div>
+                        <IoIosArrowForward />
+                        <p>Others</p>
+                    </div>
+                </div>
+
+                <div className={`headercategory ${headerSub ? 'fade-in' : ''}`}>
+                <div className='backtomain' onClick={toggleHeaderSub}>
+                    <IoIosArrowBack className="leftarrow"/>
+                    <h5>Back to Categories</h5>
+                </div>
+
+                <div className='trainingtypes'>
+                    <div className='trainingtype'>
+                        <div className="imagetraining">
+                            <img src={adaptech} alt="" />
+                        </div>
+                        <div className="texttraining">
+                            <h4>Adaptech L.L.C</h4>
+                            <h5>ICT & Technology</h5>
+                        </div>
+                    </div>
+
+                    <div className='trainingtype'>
+                        <div className="imagetraining">
+                            <img src={hangar} alt="" />
+                        </div>
+                        <div className="texttraining">
+                            <h4>Autostrada Hangar</h4>
+                            <h5>Creative & Culture</h5>
+                        </div>
+                    </div>
+
+                    <div className='trainingtype'>
+                        <div className="imagetraining">
+                            <img src={cacttus} alt="" />
+                        </div>
+                        <div className="texttraining">
+                            <h4>Cacttus Education</h4>
+                            <h5>Education & Trainings</h5>
+                        </div>
+                    </div>
+
+                    <div className='trainingtype'>
+                        <div className="imagetraining">
+                            <img src={eizek} alt="" />
+                        </div>
+                        <div className="texttraining">
+                            <h4>Eizek Mechatronics</h4>
+                            <h5>ICT & Technology</h5>
+                        </div>
+                    </div>
+
+                    <div className='trainingtype'>
+                        <div className="imagetraining">
+                            <img src={makerspace} alt="" />
+                        </div>
+                        <div className="texttraining">
+                            <h4>Makerspace Innovation Prizren</h4>
+                            <h5>ICT & Technology</h5>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </header>
 
