16733c56004a1461400c58554f0d0227d4dac0ea
AlmirPinduk <78119073+AlmirPinduku@users.noreply.github.com>
2024-04-01 13:36:45 +0200

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import $ from "jquery";
 import MobileMainPage from "./components/MobileMainPage/MobileMainPage";
 import RedBuildingFloor0 from "./components/MobileRedBuildingFloor0/RedBuildingFloor0";
 
